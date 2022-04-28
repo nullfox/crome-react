5608f2300a648d436e577b5a83c41e84e9e25f2e
Ben Fox <ben@lattice.com>
2022-04-28 10:07:44 -0700

    Fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,63 @@
-import { Box, Flex, Heading, List, ListItem } from '@chakra-ui/react';
+import { Box, Flex, Heading, List, ListItem, Text } from '@chakra-ui/react';
 import { ethers } from 'ethers';
 import type { NextPage } from 'next';
-import dynamic from 'next/dynamic';
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
-import { TokenState, useWalletContext } from '../contexts/Wallet';
+import { useWalletContext } from '../contexts/Wallet';
+import { useWalletTokensLazyQuery } from '../graphql/graphql';
 
 const Home: NextPage = () => {
   const router = useRouter();
-  const { tokens, tokenState } = useWalletContext();
 
-  console.log(tokenState, tokens);
+  const { currentAccount } = useWalletContext();
+
+  const [getTokens, { data, loading }] = useWalletTokensLazyQuery();
+
+  useEffect(() => {
+    if (currentAccount) {
+      getTokens({
+        variables: {
+          address: currentAccount,
+        },
+      });
+    }
+  }, [currentAccount]);
+
+  if (!data || loading) {
+    return null;
+  }
 
   return (
     <Flex flexDir="column" w="50%" mx="auto" mt={10}>
       <Heading textAlign="center">My Tokens</Heading>
 
-      {tokenState === TokenState.LOADED && (
-        <List pt={6}>
-          {tokens.map((token) => (
-            <ListItem
-              key={token.address}
-              _hover={{ cursor: 'pointer' }}
-              onClick={() => router.push(`/token/${token.address}`)}
-            >
-              <Box>{token.symbol}</Box>
-              <Box>{ethers.utils.formatEther(token.quantity)}</Box>
-            </ListItem>
-          ))}
-        </List>
-      )}
+      <List pt={6}>
+        {data.wallet.tokens.map((token) => (
+          <ListItem
+            key={token.address}
+            _hover={{ cursor: 'pointer' }}
+            onClick={() => router.push(`/token/${token.address}`)}
+          >
+            <Box>{token.symbol}</Box>
+            <Box>
+              <Text fontWeight="600">
+                $
+                {(+ethers.utils.formatUnits(
+                  token.usdcTotal,
+                  token.decimals,
+                )).toFixed(2)}
+              </Text>
+              <Text>
+                {(+ethers.utils.formatUnits(
+                  token.balance,
+                  token.decimals,
+                )).toFixed(4)}
+              </Text>
+            </Box>
+          </ListItem>
+        ))}
+      </List>
     </Flex>
   );
 };
